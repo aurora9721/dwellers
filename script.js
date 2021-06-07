3c90ca718e054c12cc1876c6c260a0b0c673872b
aurora9721 <55090088+aurora9721@users.noreply.github.com>
2021-06-07 15:40:03 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// LANDING PAGE
+
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+//
+// function myFunction() {
+//   var winScroll = document.body.scrollLeft || document.documentElement.scrollLeft;
+//   var width = document.documentElement.scrollWidth - document.documentElement.clientWidth;
+//   var scrolled = (winScroll / width) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
